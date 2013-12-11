f1ae29d393cb1454d1d2566a9bfe8b28815c8968
Kimberly Narine <kimberlyonarine@gmail.com>
2013-12-11 12:43:20 -0500

    Inital commit, new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodReadsClone::Application.config.secret_token = 'b84c2da2ec178cf1934d3ad678c58d9ff0934f984562733518a60044e18191653d18f5d5629df0778aa22c167722123ff94f8f23e35f16d7db3a3c5f9eeac130'
