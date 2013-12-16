b88f8339333bad3e9fef5800892dae342e2a8e31
Kimberly Narine <kimberlyonarine@gmail.com>
2013-12-16 12:53:01 -0500

    added css files to assets.precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ GoodReadsClone::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( search.js home.css admins.css books.css)
+  config.assets.precompile += %w( search.js home.css admins.css books.css clubs.css comments.css home.css likes.css posts.css sessions.css users.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
